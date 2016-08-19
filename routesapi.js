1984bf24e79a8594318dc998785a7a647cd31e71
Eamonn de Leastar <edeleastar@wit.ie>
2016-08-19 09:09:41 +0100

    get candidate endpoint

--- routesapi.js
+++ routesapi.js
@@ -2,4 +2,5 @@ const CandidatesApi = require('./app/api/candidatesapi');
 
 module.exports = [
   { method: 'GET', path: '/api/candidates', config: CandidatesApi.find },
+  { method: 'GET', path: '/api/candidates/{id}', config: CandidatesApi.findOne },
 ];
