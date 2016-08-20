0d4daf2696e8fa19835241ada5196bd1a0db8d11
Eamonn de Leastar <edeleastar@wit.ie>
2016-08-20 20:24:53 +0100

    bug fix in userapi - wrong case for create user route

--- app/api/usersapi.js
+++ app/api/usersapi.js
@@ -40,8 +40,8 @@ exports.create = {
   auth: false,
 
   handler: function (request, reply) {
-    const User = new User(request.payload);
-    User.save().then(newUser => {
+    const user = new User(request.payload);
+    user.save().then(newUser => {
       reply(newUser).code(201);
     }).catch(err => {
       reply(Boom.badImplementation('error creating User'));
