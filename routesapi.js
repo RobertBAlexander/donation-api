eb7003f5214aea2edc1c3f45d43c0aea863e5afa
Eamonn de Leastar <edeleastar@wit.ie>
2016-08-20 07:05:36 +0100

    lab.12.exercises

--- routesapi.js
+++ routesapi.js
@@ -1,4 +1,5 @@
 const CandidatesApi = require('./app/api/candidatesapi');
+const UsersApi = require('./app/api/usersapi');
 
 module.exports = [
   { method: 'GET', path: '/api/candidates', config: CandidatesApi.find },
@@ -6,4 +7,10 @@ module.exports = [
   { method: 'POST', path: '/api/candidates', config: CandidatesApi.create },
   { method: 'DELETE', path: '/api/candidates/{id}', config: CandidatesApi.deleteOne },
   { method: 'DELETE', path: '/api/candidates', config: CandidatesApi.deleteAll },
+
+  { method: 'GET', path: '/api/users', config: UsersApi.find },
+  { method: 'GET', path: '/api/users/{id}', config: UsersApi.findOne },
+  { method: 'POST', path: '/api/users', config: UsersApi.create },
+  { method: 'DELETE', path: '/api/users/{id}', config: UsersApi.deleteOne },
+  { method: 'DELETE', path: '/api/users', config: UsersApi.deleteAll },
 ];
