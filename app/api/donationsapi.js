40c18ab24639f7037381320372deb47fc4f25882
Eamonn de Leastar <edeleastar@wit.ie>
2016-08-21 14:40:46 +0100

    make donation and delete all donation handlers (+auth fix)

--- app/api/donationsapi.js
+++ app/api/donationsapi.js
@@ -5,6 +5,8 @@ const Boom = require('boom');
 
 exports.findAllDonations = {
 
+  auth: false,
+
   handler: function (request, reply) {
     Donation.find({}).populate('donor').populate('candidate').then(donations => {
       reply(donations);
@@ -16,6 +18,8 @@ exports.findAllDonations = {
 
 exports.findDonations = {
 
+  auth: false,
+
   handler: function (request, reply) {
     Donation.find({ candidate: request.params.id }).then(donations => {
       reply(donations);
@@ -24,4 +28,33 @@ exports.findDonations = {
     });
   },
 
+};
+
+exports.makeDonation = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    const donation = new Donation(request.payload);
+    donation.candidate = request.params.id;
+    donation.save().then(newDonation => {
+      reply(newDonation).code(201);
+    }).catch(err => {
+      reply(Boom.badImplementation('error making donation'));
+    });
+  },
+
+};
+
+exports.deleteAllDonations = {
+
+  auth: false,
+
+  handler: function (request, reply) {
+    Donation.remove({}).then(err => {
+      reply().code(204);
+    }).catch(err => {
+      reply(Boom.badImplementation('error removing Users'));
+    });
+  },
 };
