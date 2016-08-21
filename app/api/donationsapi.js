aa4c7f85fdf05688b065523e2bcdc2f8b539be5b
Eamonn de Leastar <edeleastar@wit.ie>
2016-08-21 08:44:59 +0100

    retrieve all donations api

---
+++ app/api/donationsapi.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Donation = require('../models/donation');
+const Boom = require('boom');
+
+exports.findAllDonations = {
+
+  handler: function (request, reply) {
+    Donation.find({}).populate('donor').populate('candidate').then(donations => {
+      reply(donations);
+    }).catch(err => {
+      reply(Boom.badImplementation('error accessing db'));
+    });
+  },
+};
