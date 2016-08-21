e4204b48fb3c7d85f4fd88f53c821c86539a0a92
Eamonn de Leastar <edeleastar@wit.ie>
2016-08-21 19:26:58 +0100

    authenticate route

--- routesapi.js
+++ routesapi.js
@@ -20,4 +20,6 @@ module.exports = [
   { method: 'POST', path: '/api/candidates/{id}/donations', config: DonationsApi.makeDonation },
   { method: 'DELETE', path: '/api/candidates/{id}/donations', config: DonationsApi.deleteDonations },
   { method: 'DELETE', path: '/api/donations', config: DonationsApi.deleteAllDonations },
+
+  { method: 'POST', path: '/api/users/authenticate', config: UsersApi.authenticate },
 ];
