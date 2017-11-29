4f6a33126bc57215451d4fedc455a891c9ff6c46
Robert Alexander <robert.b.alexander@gmail.com>
2017-11-29 12:18:37 +0000

    First commit.

--- app/models/db.js
+++ app/models/db.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
@@ -20,12 +20,52 @@ mongoose.connection.on('connected', function () {
     const Candidate = require('./candidate.js');
     seeder.seed(data, { dropDatabase: false, dropCollections: true }).then(dbData => {
       console.log('preloading Test Data');
+    console.log(dbData);
+  }).catch(err => {
+      console.log(error);
+  });
+  }
+});
+
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});*/
+
+/**
+ * Created by Robert Alexander on 16/10/2017.
+ */
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+let dbURI = 'mongodb://hoothootuser:secret@ds121565.mlab.com:21565/donation';
+//let dbURI = 'mongodb://localhost/hoot';
+//if (process.env.NODE_ENV === 'production') {
+// dbURI = process.env.MONGODB_URI;
+//}
+
+mongoose.connect(dbURI);
+
+/*mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+  if (process.env.NODE_ENV != 'production') {
+    var seeder = require('mongoose-seeder');
+    //const data = require('./data.json');
+    const Hoot = require('./hoot');
+    const User = require('./user');
+    seeder.seed(Hoot, User, { dropDatabase: false, dropCollections: true }).then(dbData => {
+      console.log('preloading Test Data');
       console.log(dbData);
     }).catch(err => {
       console.log(error);
     });
   }
-});
+});*/
 
 mongoose.connection.on('error', function (err) {
   console.log('Mongoose connection error: ' + err);
